895dba7cf585c1d6b3614e61c5ca37309a6925c2
EC2 Default User <ec2-user@ip-172-31-80-11.ec2.internal>
2022-04-20 23:39:01 +0000

    bootstrap,devise導入・topページ作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "1964aefe1082495d95d127af093f7a2e.vfs.cloud9.us-east-1.amazonaws.com"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
