490b7806d2b6129cc366196bcc00fafe6cca0ee4
EC2 Default User <ec2-user@ip-172-31-80-11.ec2.internal>
2022-05-02 01:33:18 +0000

    サイドアイコンの追加・レイアウト調整

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:content)
   end
 end
