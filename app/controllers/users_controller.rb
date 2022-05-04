3fddfc19b04b1118fa6d4b68497f37084a75b247
“INatsukiI“ <e1q20019@oit.ac.jp>
2022-05-04 01:57:13 +0000

    プロフィール編集画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @articles = @user.articles
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
