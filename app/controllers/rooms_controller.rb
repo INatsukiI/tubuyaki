f151d4801ac285614fbb8c8708b142922ed18d12
INatsukiI <e1q20019@oit.ac.jp>
2022-05-27 10:46:44 +0000

    dm機能途中・roomscontrollerまで作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,18 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
+    room = Room.create
+    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    redirect_to room_path(room)
   end
 
   def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages.all
+    @message = Message.new
+    @entries = @room.entries
+    @another_entry = @entries.where.not(user_id: current_user.id).first
   end
 end
