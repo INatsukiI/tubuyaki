60d609f8455ab7dd150fb45248e8ad02dad8a00d
INatsukiI <e1q20019@oit.ac.jp>
2022-05-31 08:34:18 +0000

    roomsコントローラーindexアクション追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,9 +3,19 @@ class RoomsController < ApplicationController
 
   def create
     room = Room.create
-    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
-    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    @current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
     redirect_to room_path(room)
+  end
+  
+  def index
+    current_entries = current_user.entries
+    my_room_id = []
+    current_entries.each do |entry|
+      my_room_id << entry.room.id
+    end
+    
+    @another_entries = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
   end
 
   def show
