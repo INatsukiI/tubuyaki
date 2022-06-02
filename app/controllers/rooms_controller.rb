524ebd9162fc3edf51119709779cdadd768b6d59
INatsukiI <e1q20019@oit.ac.jp>
2022-06-02 14:40:13 +0000

    dm機能・メッセージコントローラー作成前まで

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,27 +2,27 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    room = Room.create
-    @current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
-    @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
-    redirect_to room_path(room)
+    @room = Room.create
+    @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
+    @another_entry = Entry.create(entry_params).merge(room_id: @room.id)
+    redirect_to room_path(@room)
   end
   
-  def index
-    current_entries = current_user.entries
-    my_room_id = []
-    current_entries.each do |entry|
-      my_room_id << entry.room.id
-    end
-    
-    @another_entries = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
-  end
-
   def show
     @room = Room.find(params[:id])
-    @messages = @room.messages.all
-    @message = Message.new
-    @entries = @room.entries
-    @another_entry = @entries.where.not(user_id: current_user.id).first
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_back(fallback_location: root_path)
+    end
+    #@another_entry = @entries.where.not(user_id: current_user.id).first
+  end
+  
+  
+  private
+  def entry_params
+    params.require(:entry).permit(:user_id, :room_id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ApplicationRecord
-  has_many :entrie, dependent: :destroy
+  has_many :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
 end
